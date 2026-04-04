6e9f30bf2b427fc9a9a188b664e5da9e6da6a5aa
Elian Rehbani <elianrhbn@gmail.com>
2026-04-04 16:38:53 -0300

    first commit

---
+++ PracticaSpringWeb-master/PracticaSpringWeb-master/src/main/java/org/eduardomango/practicaspringweb/model/controllers/UserController.java
@@ -0,0 +1,64 @@
+package org.eduardomango.practicaspringweb.model.controllers;
+
+
+import org.apache.coyote.Response;
+import org.eduardomango.practicaspringweb.model.entities.SaleEntity;
+import org.eduardomango.practicaspringweb.model.entities.UserEntity;
+import org.eduardomango.practicaspringweb.model.repositories.UserRepository;
+import org.eduardomango.practicaspringweb.model.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+    UserRepository userRepository = new UserRepository();
+    UserService userService = new UserService(userRepository);
+
+    @GetMapping
+    public List<UserEntity> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserEntity> findById(@PathVariable int id) {
+        UserEntity user = userService.findById(id);
+        if(user != null) return ResponseEntity.ok(user);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    public ResponseEntity<UserEntity> save(@RequestBody UserEntity newUser){
+        if( newUser == null ||
+            newUser.getUsername() == null || newUser.getUsername().isBlank() ||
+            newUser.getEmail() == null || newUser.getEmail().isBlank() ||
+            newUser.getPassword() == null || newUser.getPassword().isBlank()){
+            return ResponseEntity.badRequest().build();
+        }
+        userService.save(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public String edit(@RequestBody UserEntity newUser, @PathVariable int id) {
+        System.out.println(id);
+        System.out.println(newUser);
+        userService.update(newUser, id);
+        return "Usuario " + id + " actualizado";
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        UserEntity userToDelete = userService.findById(id);
+        if(userToDelete == null)  return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        userService.delete(userToDelete);
+        return ResponseEntity.ok("Usuario eliminado correctamente");
+
+    }
+
+
+}
---
+++ PracticaSpringWeb-master/PracticaSpringWeb-master/src/main/java/org/eduardomango/practicaspringweb/model/services/SaleService.java
@@ -0,0 +1,70 @@
+package org.eduardomango.practicaspringweb.model.services;
+import org.eduardomango.practicaspringweb.model.entities.ProductEntity;
+import org.eduardomango.practicaspringweb.model.entities.SaleEntity;
+import org.eduardomango.practicaspringweb.model.entities.UserEntity;
+import org.eduardomango.practicaspringweb.model.exceptions.SaleNotFoundException;
+import org.eduardomango.practicaspringweb.model.repositories.IRepository;
+import org.eduardomango.practicaspringweb.model.repositories.SaleRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+
+@Service
+public class SaleService {
+    private final SaleRepository saleRepository;
+    private final ProductService productService;
+    private final UserService userService;
+
+    public SaleService(IRepository<SaleEntity> saleRepository, SaleRepository saleRepository1, ProductService productService, UserService userService) {
+        this.saleRepository = saleRepository1;
+        this.productService = productService;
+        this.userService = userService;
+    }
+
+
+    public List<SaleEntity> findAll() {
+        return saleRepository.findAll();
+    }
+
+    public SaleEntity findById(int idSale) {
+        return saleRepository.findAll()
+                .stream()
+                .filter(s -> s.getId() == idSale)
+                .findFirst()
+                .orElseThrow(SaleNotFoundException::new);
+    }
+
+
+    public SaleEntity registerSale (int idCliente, int idProducto, Long quantity){
+        ProductEntity product = productService.findById(idProducto);
+        UserEntity user = userService.findById(idCliente);
+
+        SaleEntity sale = SaleEntity.builder()
+                .id(System.currentTimeMillis())
+                .products(product)
+                .client(user)
+                .quantity(quantity)
+                .saleDate(LocalDate.now())
+                .build();
+
+        saleRepository.save(sale);
+        System.out.println("se guardo kumpa");
+        return sale;
+    }
+
+    public void deleteSale(int idSale) {
+        SaleEntity sale = findById(idSale);
+        saleRepository.delete(sale);
+    }
+
+    public void setSale(SaleEntity newSale, SaleEntity id) {
+//        SaleEntity sale = findById(id);
+  //      saleRepository.update(newSale, id);
+    }
+
+
+}
